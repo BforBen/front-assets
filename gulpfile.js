918d81f0a5695f529399a00a4c5f8197103cf6c3
Ben <ben@cheetham.me.uk>
2015-02-11 13:13:54 +0000

    Initial commit of Gulp

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+		csslint = require('gulp-csslint'),
+        del = require('del'),
+		gutil = require('gulp-util'),
+		less = require('gulp-less'),
+        minifyCSS = require('gulp-minify-css'),
+        sourcemaps = require('gulp-sourcemaps');
+ 
+ var customReporter = function(file) {
+  gutil.log(gutil.colors.cyan(file.csslint.errorCount)+' errors in '+gutil.colors.magenta(file.path));
+
+  file.csslint.results.forEach(function(result) {
+    gutil.log(result.error.message+' on line '+result.error.line);
+  });
+};
+ 
+ gulp.task('build-less', function(){
+  gulp.src('./less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist/'));
+});
+ 
+ gulp.task('copy:images', function(){
+  gulp.src('./images/*.(gif|jpg|png)')
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('lint:css', function() {
+  gulp.src('./css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.reporter(customReporter));
+});
+ 
+gulp.task('minify:css', ['clean:dist', 'lint:css'], function() {
+  gulp.src('./css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('clean:dist', function (cb) {
+  del([
+    'dist/*'
+  ], cb);
+});
+
+
+gulp.task('default', ['build-less', 'copy:images', 'minify:css']);
